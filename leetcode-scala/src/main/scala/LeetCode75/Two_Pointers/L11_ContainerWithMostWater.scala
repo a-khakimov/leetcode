5a61e16375901fb0e8d120f142fd6811629d47a8
a-khakimov <techlinked@yandex.ru>
2025-01-06 14:00:02 +0500

    L11_ContainerWithMostWater_v2

--- leetcode-scala/src/main/scala/LeetCode75/Two_Pointers/L11_ContainerWithMostWater.scala
+++ leetcode-scala/src/main/scala/LeetCode75/Two_Pointers/L11_ContainerWithMostWater.scala
@@ -2,11 +2,14 @@ package LeetCode75.Two_Pointers
 
 object L11_ContainerWithMostWater extends App {
   object Solution {
-    def maxArea(height: Array[Int]): Int = {
+
+    // bad solution
+    def maxArea2(height: Array[Int]): Int = {
       val len = height.length
       var a = 0
       var b = 1
       var maxArea = 0
+
       while (a < len) {
         while (b < len) {
           val area = (b - a) * Math.min(height(a), height(b))
@@ -17,6 +20,42 @@ object L11_ContainerWithMostWater extends App {
         }
         a += 1
         b = a + 1
+      }
+      maxArea
+    }
+
+    def maxArea3(height: Array[Int]): Int = {
+      var a = 0
+      var b = height.length - 1
+      var maxArea = 0
+      while (a < b) {
+        maxArea = Math.max(Math.min(height(a), height(b)) * (b - a), maxArea)
+        if (height(a) < height(b)) {
+          val cur = a
+          while (a < b && height(cur) >= height(a)) {
+            a += 1
+          }
+        } else {
+          val cur = b
+          while (a < b && height(cur) >= height(b)) {
+            b -= 1
+          }
+        }
+      }
+      maxArea
+    }
+
+    def maxArea(height: Array[Int]): Int = {
+      var a = 0
+      var b = height.length - 1
+      var maxArea = 0
+      while (a < b) {
+        maxArea = Math.max(Math.min(height(a), height(b)) * (b - a), maxArea)
+        if (height(a) < height(b)) {
+          a += 1
+        } else {
+          b -= 1
+        }
       }
       maxArea
     }
